f0c8d76f6ccc96767c7f6a53f625c5f61b8c824b
Nick Noble <nicholas.noble@foundry.com>
2022-10-20 13:41:00 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+// document.addEventListener('mousemove', (e) => {
+//     const xPercent = parseInt(e.pageX / window.innerWidth*360);
+//     const yPercent = parseInt(e.pageY / window.innerHeight*360);
+//     document.documentElement.style.setProperty('--mouse-x', `${xPercent}deg`);
+//     document.documentElement.style.setProperty('--mouse-y', `${yPercent}deg`);
+//     const mouseX = parseInt(e.pageX / window.innerWidth * 255);
+//     document.documentElement.style.setProperty(
+//         '--background', `linear-gradient(45deg, rgb(${mouseX},${mouseX},${mouseX}),#1F1B4E)`);
+// });
